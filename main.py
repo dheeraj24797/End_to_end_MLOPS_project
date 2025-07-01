c0c913f2e384536c1c4e4dfd25969f7de5981b92
dheeraj24797 <dheeraj.sree222@gmail.com>
2025-07-01 16:04:33 -0500

    "Code updates"

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from MLOPS_project import logger
 from MLOPS_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
+
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
--- src/MLOPS_project/pipeline/stage_01_data_ingestion.py
+++ src/MLOPS_project/pipeline/stage_01_data_ingestion.py
@@ -2,6 +2,8 @@ from MLOPS_project.config.configuration import ConfigurationManager
 from MLOPS_project.components.data_ingestion import DataIngestion
 from MLOPS_project import logger
 
+
+
 STAGE_NAME = "Data Ingestion stage"
 
 class DataIngestionTrainingPipeline:
@@ -16,7 +18,7 @@ class DataIngestionTrainingPipeline:
         data_ingestion.extract_zip_file()
 
 
-
+    
 if __name__ == '__main__':
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
