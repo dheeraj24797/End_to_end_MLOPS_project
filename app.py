3b56e4dd571e2289d40dda8da13dc24c4f01b236
dheeraj24797 <dheeraj.sree222@gmail.com>
2025-07-14 21:41:29 -0500

    'Dockerfile'

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import pandas as pd
 from MLOPS_project.pipeline.prediction import PredictionPipeline
+from flask import send_from_directory
 
 
 app = Flask(__name__) # initializing a flask app
@@ -50,6 +51,12 @@ def index():
 
     else:
         return render_template('index.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 if __name__ == "__main__":
